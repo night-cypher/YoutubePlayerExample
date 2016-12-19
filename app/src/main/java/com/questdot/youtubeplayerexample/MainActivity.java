0f328fabbc029adddfcbf6d74c4c95e32c7add39
HP <krist.ng@corous360.com>
2016-12-19 23:59:51 +0800

    change

--- app/src/main/java/com/questdot/youtubeplayerexample/MainActivity.java
+++ app/src/main/java/com/questdot/youtubeplayerexample/MainActivity.java
@@ -1,13 +1,12 @@
 package com.questdot.youtubeplayerexample;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-
-public class MainActivity extends YouTubeBaseActivity {
+public class MainActivity extends Activity {
 
     Button btnActivity;
     Button btnFragment;
